1a5ad39283c152bfd4fd85c06f94bec103ad159e
u1and0 <e01.ando60@gmail.com>
2024-01-30 10:48:42 +0900

    [docs] README, docstring

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+<img src="https://img.shields.io/badge/version-v0.1.0-FF7777.svg"></img>
+<img src="https://img.shields.io/badge/LICENSE-MIT-3388FF.svg"></img>
+
 Disc Usage Monitor
 =======
 
@@ -12,4 +15,4 @@ visualize the data using plotly-dash.
 uvicorn --host "0.0.0.0" --port 8881 main:app
 ```
 
-Open browser and access 'localhost:8881'
+Open browser and access 'localhost:8881/index'
--- main.py
+++ main.py
@@ -1,42 +1,32 @@
+"""
+Save the data to a SQLite3 database by periodically hitting the df command,
+visualize the data using plotly-dash.
+
+Usage:
+
+```
+uvicorn --host "0.0.0.0" --port 8881 main:app
+```
+
+Open browser and access 'localhost:8881/index'
+"""
 from dashapp import create_dash_app
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
-# from fastapi.staticfiles import StaticFiles
-# import asyncio
 import uvicorn
 
 from dashapp import db_init, create_dash_app
 
+VERSION = "v0.1.0"
 app = FastAPI()
-
-# def plot_graph():
-#     df = pd.read_csv("disk_usage.csv")
-#     cf.set_config_file(theme='ggplot')
-#     df.iplot(kind='scatter', x='Timestamp', y='Used')
-#
-#     return cf.iplot(df, filename='disk-usage')
-
-# @app.get("/")
-# async def root():
-#     conn = sqlite3.connect("disk_usage.db")
-#     df = pd.read_sql_query(f"SELECT * FROM {TABLE_NAME}", conn)
-#     conn.close()
-#
-#     cf.set_config_file(theme="ggplot")
-#     df.iplot(kind="scatter", x="timestamp", y="used")
-#     return cf.iplot(df, filename="disk-usage")
-
-# async def main_loop():
-#     while True:
-#         data = run_df()
-#         print(data)
-#         await asyncio.sleep(6)
-
 
 @app.get("/")
 def read_main():
     return {
         "routes": [
+            {
+                "version": VERSION,
+            },
             {
                 "method": "GET",
                 "path": "/",
